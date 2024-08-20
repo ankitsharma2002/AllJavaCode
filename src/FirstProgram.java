9a77e515a987347a16d6dfa3603667625061c750
Ankit Sharma <ankitsharma8102002@gmail.com>
2024-08-20 14:51:29 +0530

    Commit for the 2-=08-2024

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -2,7 +2,31 @@
 public class FirstProgram {
 	public static void main(String args[]) {
 		System.out.println("Hello World");
+		
 		char ch = 'a';
 		System.out.println(ch + 1);
+		
+		int a = 1000000;
+		System.out.println((a++) + "" + ((a--)));
+		System.out.println(a*(a+1)/2);
+		
+// How negative number are stored in memory . 
+		
+		int b=3;
+		b=+3;
+		System.out.println(b);
+		System.out.println(null==null);
+		
+		int ab=5;
+		if((ab&1)!=0) {
+			System.out.println("Odd");
+		}
+		else {
+			System.out.println("Even");
+		}
+		
+		System.out.println(~2);
+		
+		int arr[]= {}
 	}
 }
